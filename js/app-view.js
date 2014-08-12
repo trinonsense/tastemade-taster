059af7b61ec28ca3a79c9d3fb62c97e5f77473ad
Tri Noensie <tnnoensie@gmail.com>
2014-08-11 23:00:56 -0500

    taster thumbnail markup, style, and data

--- js/app-view.js
+++ js/app-view.js
@@ -8,9 +8,19 @@ define([
 		el: '.app',
 
 		initialize: function() {
-			this.collection.on('sync', function(tasters) {
-				this.$('.taster img').attr('src', tasters.at(0).get('thumbnail').url);
-			}, this);
+			this.collection.on('sync', this.render, this);
+		},
+
+		render: function(tasters) {
+			var taster = tasters.at(0);
+
+			this.$('.taster')
+				.find('.data-thumbnail').attr('src', taster.get('thumbnail').url).end()
+				.find('.data-venue').text(taster.get('venue').name).end()
+				.find('.data-user').text(taster.get('creator').name).end()
+				.find('.data-likers').text(taster.get('stats').likers).end()
+				.find('.data-comments').text(taster.get('stats').comments).end()
+				.find('.data-views').text(taster.get('stats').views).end();
 		}
 	});
 
