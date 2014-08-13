08745864b4474da03fe2dc34a73c10ef2da75650
Tri Noensie <tnnoensie@gmail.com>
2014-08-12 22:09:19 -0500

    video players

--- js/app-view.js
+++ js/app-view.js
@@ -7,20 +7,39 @@ define([
 	var AppView = Backbone.View.extend({
 		el: '.app',
 
+		events: {
+			'click .taster-video-player' : 'playPause'
+		},
+
 		initialize: function() {
 			this.collection.on('sync', this.render, this);
+		},
+
+		playPause: function() {
+			var video = this.$('.taster-video')[0];
+
+			if (video.paused) {
+				video.play();
+			} else {
+				video.pause();
+			}
+
+			this.$('.taster-video-player').toggleClass('is-paused');
 		},
 
 		render: function(tasters) {
 			var taster = tasters.at(0);
 
 			this.$('.taster')
-				.find('.data-thumbnail').attr('src', taster.get('thumbnail').url).end()
 				.find('.data-venue').text(taster.get('venue').name).end()
 				.find('.data-user').text(taster.get('creator').name).end()
 				.find('.data-likers').text(taster.get('stats').likers).end()
 				.find('.data-comments').text(taster.get('stats').comments).end()
-				.find('.data-views').text(taster.get('stats').views).end();
+				.find('.data-views').text(taster.get('stats').views).end()
+				.find('.data-video').attr({
+					poster: taster.get('thumbnail').url,
+					src: taster.get('video').url
+				});
 		}
 	});
 
